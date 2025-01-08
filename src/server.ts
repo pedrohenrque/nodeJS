c8b0e6f58fb3a76132b1d641c28ff84b08c3630c
Pedro Hennrique <pedrosilveira07@outlook.com>
2025-01-08 10:55:29 -0300

    init project

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import fastify from 'fastify'
+import crypto from 'node:crypto'
+import { knex } from './database'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Trasação de teste',
+      amount: 1000
+    })
+    .returning('*')
+
+  return transaction
+})
+
+app
+  .listen({
+    port: 3333
+  })
+  .then(() => {
+    console.log('HTTP Server Running')
+  })
